f57e2a6494ce3ed4c071f8c3be1bed251bb96376
andresalinas18 <afsalinas18@gmail.com>
2025-05-12 11:35:46 -0500

    feat: implement chained operations logic in calculator

--- index.js
+++ index.js
@@ -1,23 +1,102 @@
-// 1. wait until DOM
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('App iniciada');
+// Create variables
+let firstOperand = null;
+let secondOperand = null;
+let currentOperator = null;
+let waitingForSecondOperand = false;
+
+// call variablesfrom the HTML
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll(".pad button");
+
+// logic of each button
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+   const value = button.textContent;
+   
   
-    // 2. initialize components
-    initButtons();
-    loadData();
+   if (value === "c") {
+   display.textContent = "";
+   firstOperand = null;
+   secondOperand = null;
+   currentOperator = null;
+   waitingForSecondOperand = false;
+  }
+
+  else if (value === "="){
+    if (firstOperand !== null && currentOperator && !waitingForSecondOperand){
+      secondOperand = parseFloat(display.textContent);
+      const result = operate(currentOperator, firstOperand, secondOperand);
+      display.textContent = result;
+      currentOperator = null;
+      waitingForSecondOperand = true;
+  };
+  }
+
+  else if (["+", "-", "*", "/"].includes(value)) {
+    
+    if (firstOperand === null) {
+      firstOperand = parseFloat(display.textContent);
+      currentOperator = value;
+      waitingForSecondOperand = true;
+  
+    
+    } else if (!waitingForSecondOperand) {
+      secondOperand = parseFloat(display.textContent);
+      const result = operate(currentOperator, firstOperand, secondOperand);
+      display.textContent = result;
+  
+      firstOperand = result;
+      currentOperator = value;
+      waitingForSecondOperand = true;
+    } else {
+      
+      currentOperator = value;
+    }
+  }
+  
+
+     else {
+      if(waitingForSecondOperand) {
+        display.textContent = value;
+        waitingForSecondOperand = false;
+        
+      } else {
+        display.textContent +=value;
+      };      
+    }
   });
-  
-  // 3. main funcions
-  function initButtons() {
-    const button = document.querySelector('#myButton');
-    button.addEventListener('click', () => {
-      alert('Botón clickeado!');
-    });
-  }
-  
-  function loadData() {
-    // Example: Fetch API to load data
-    fetch('https://api.example.com/data')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }
+});
+
+// create operand functions
+
+function operate(operator, a, b){
+  if (operator === "+"){return add(a,b) };
+  if (operator === "-"){return substract(a,b) };
+  if (operator === "/"){return divide(a,b) };
+  if (operator === "*"){return multiply(a,b) }
+
+};
+
+function add(a,b){
+  const c = a + b ;
+  return c;
+};
+
+function substract(a,b){
+  const c = a - b;
+  return c;
+};
+
+function divide(a,b){
+  const c = a / b;
+  if (b === 0){ 
+    return "Divisions by zero is undefined" 
+  } else {
+    return c;
+  };
+};
+
+function multiply(a,b){
+  const c = a * b;
+  return c;
+};  
